a50d282792a875194667da02b3b4250d96b76355
A-Mckinlay <mckinlayallan@gmail.com>
2020-07-12 17:00:39 +0100

    Added test for the GetCode method.

---
+++ ShrtnrTests/Hashids/HasherTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Shrtnr.HashIds;
+using Xunit;
+
+namespace ShrtnrTests.Hashids
+{
+    public class HasherTests
+    {
+        #region GetCode()
+        [Fact]
+        public void ItShouldReturnACodeWithAtLeast6Characters()
+        {
+            var hasher = new Hasher("Some salt");
+            var code = hasher.GetCode();
+
+            Assert.IsType<string>(code);
+            Assert.True(code.Length >= 6);
+        }
+
+        #endregion
+    }
+}
